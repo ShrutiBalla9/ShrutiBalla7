6ad7ac6b193d10b718a8a9152d2f824c0e0457b4
ShrutiBalla9 <88773345+ShrutiBalla9@users.noreply.github.com>
2021-08-14 16:29:56 +0530

    Datalist.py

---
+++ operators.py
@@ -0,0 +1,20 @@
+# Arithmetic operator
+x=20
+y=10
+print("\n 20+10=",x+y)
+print("\n 20-10=",x-y)
+print("\n 20*10=",x*y)
+print("\n 20%10=",x%y)
+print("\n 20/10=",x/y)
+print("\n 20**10=",pow(x,y))
+
+#comparison operators
+print("x>y",x>y)
+print("x<y",x<y)
+print("x==y",x==y)
+print("x!=y",x!=y)
+
+#logical operator
+print("x and y is",x and y)
+print("x or y is",x or y)
+print("x not y is", not y)
